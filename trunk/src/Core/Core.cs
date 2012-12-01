cfe59f8588e16bb78041ec2aa58b869eb20120df
yanchev.lyubomir@gmail.com <yanchev.lyubomir@gmail.com@01aef698-5dda-9de8-0d9f-6f5f9fcb2882>
2012-12-01 00:27:03 +0000

    Tests almost finished, only "Taekwondo position" left. After finishing all the tests, I have to copy the table into the documentation.
    TODO:
     - write a resume in English and Bulgarian
     - organize and write the CDs for the conf.

--- trunk/src/Core/Core.cs
+++ trunk/src/Core/Core.cs
@@ -14,7 +14,7 @@ namespace Core
 		private const int SKELETON_COUNT = 6;
 		private const double ACCEPTABLE_SKELETON_SIMILARITY = 0.1;
 		private const int ACCEPTABLE_WINDOW_SIZE = 40;
-		private const double ACCEPTABLE_ACTION_SIMILARITY = 1500;
+		private const double ACCEPTABLE_ACTION_SIMILARITY = 4000;
 
 		private KinectSensor kinectSensor;
 
@@ -105,9 +105,9 @@ namespace Core
 							double overallResult = 0.0;
 							foreach (var activityRecord in activity.Recordings)
 							{
-								var activityRecordResult = DTW(activityRecord, window);
-
-								//var activityRecordResult = ElasticMatchingWithFreedomDegree.CompareActivities(activityRecord, window);
+								//var activityRecordResult = DTW(activityRecord, window);
+								
+								var activityRecordResult = ElasticMatchingWithFreedomDegree.CompareActivities(activityRecord, window);
 								overallResult += activityRecordResult;
 							}
 
